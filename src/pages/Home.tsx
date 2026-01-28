ae4e30bee8a4ac5dd275052050af8ea974420984
carter.xu <carter.xu@hansonggroup.com>
2026-01-28 09:38:34 +0800

    test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,7 +13,7 @@ export const Home = (): FunctionComponent => {
 	};
 
 	return (
-		<div className="bg-blue-300 font-bold w-screen h-screen flex flex-col justify-center items-center">
+		<div className="bg-amber-400 font-bold w-screen h-screen flex flex-col justify-center items-center">
 			<p className="text-white text-6xl">{t("home.greeting")}</p>
 			<button
 				className="hover:cursor-pointer"
